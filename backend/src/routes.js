ae11c6a05704608947920dd8aade8e145d914c3d
Junior <junior_duff_sm@hotmail.com>
2020-03-27 23:53:43 -0300

    BackEnd completo

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,23 @@
+
 const express = require('express');
+
+const OngController = require('./controllers/OngController');
+const IncidentsController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileControler')
+const SessionController = require('./controllers/SessionController')
 
 
 const routes = express.Router();
 
-routes.get('/',(req, res)=>{
-      return res.json({
-        message: 'hello'
-      });
-});
+routes.get('/profile', ProfileController.index);
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+routes.post('/session', SessionController.create)
+
+routes.post('/incidents', IncidentsController.create);
+routes.get('/incidents', IncidentsController.index);
+routes.delete('/incidents/:id', IncidentsController.delete);
+
 
 module.exports = routes;
